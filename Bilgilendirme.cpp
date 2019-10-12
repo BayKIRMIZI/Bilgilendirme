bc4c009fbf11113ce184154d5ef37d3b33682bc4
BayKIRMIZI <34891954+BayKIRMIZI@users.noreply.github.com>
2019-10-12 23:06:53 +0300

    Add files via upload

---
+++ Bilgilendirme.cpp
@@ -0,0 +1,297 @@
+1-{ OTOMASYONLAR
+	-> Müþteri alma otomasyonu{
+		- Menü Yapýsý
+			- Sýra ALma
+			- Sýradan Çýkarma
+			- Listeleme
+		- Her Ýþlemde Temizlenen Ekran
+		- Çift Yönlü Baðlý Liste
+		- Kuyruk Yapýsý
+		- Dosya Ýþlemleri
+	}
+}
+2-{ SIRALAMA ALGORÝTMALARI
+	-> Buble Sort
+	-> Insertion Sort
+	-> Merge Sort
+	-> Quick Sort
+	-> Selection Sort
+		Tüm Algoritmalar için;
+		- Sade Algoritmasý
+		- 10.000 elemanlý Rastgele	
+		- 10.000 elemanlý Düz Sýralý	
+		- 10.000 elemanlý Ters Sýralý	
+		- Çalýþma Zamaný Hesabý	
+		- Veriler Dinamik
+}
+3-{ ARAMA ALGORÝTMALARI
+	-> BFS Dosyadan{
+		- Menü Yapýsý
+			- Düðüm Derecesi
+			- Düðüm Babasý ve Kaynak Uzaklýðý
+			- Baþlangýçtan Hedefe Giden Yol
+		- Graf Yapýsý
+		- Kuyruk Yapýsý
+		- Tek Yönlü Baðlý Liste Yapýsý
+		- Veriler Dosyadan 
+	}
+	-> BFS Kullanýcýdan{
+		- Struct Fonksiyon Yapýsý
+		- Tek Yönlü Baðlý Liste Yapýsý
+		- Veriler Kullanýcýdan
+	}
+	-> DFS Dosyadan{
+		- Aðýrlýklý Graf Yapýsý
+		- Prim ALgoritmasý
+		- Veriler Dosyadan 
+		- Tek Yönlü Baðlý Liste Yapýsý
+	}
+	-> Binary Search{
+		- Dizi Yapýsý
+	}
+}
+4-{ AÐAÇLAR
+	-> Aðaç Yapýsý 1{
+		- Veriler Dinamik
+		- Struct Yapýsý
+		- Dinamik Sayý Aramasý
+		- Pre-Order , In-Order , Post-Order Algoritmalarý
+	}
+	-> Binary Tree{
+		- Menü Yapýsý
+			- Düðüm Ekle
+			- Düðüm Ara
+		- Baðlý Liste Yapýsý
+	}
+	-> Aðaçta_Arama{
+		- Çift Yönlü Baðlý Liste Yapýsý
+		- Recursive Yapý
+		- Veriler Dinamik
+	}
+	-> Aðaçta Girdiler{
+		- Menü Yapýsý
+			- Ekleme
+			- Silme
+			- Listeleme
+			- Arama
+			- Veri Miktarý
+		- Baðlý Liste Yapýsý
+		- Girdiler Kullanýcýdan
+	}
+	-> Aðaç Yapýsý 2{
+		- Veriler Kullanýcýdan
+		- Struct Yapýsý
+		- Sýralý gösterimler
+		- Arama
+	}
+}
+5-{ OTOMATA SÝSTEMLERÝ
+	-> Kural Tabanlý Sistem{
+		- Kural Dili ve Alfabesinden Kelime Üretimi
+		- Kurallar Dosyadan 
+		- Baðlý Liste Yapýsý
+		- Dosya Ýþlemleri
+	}
+}
+6-{ Baðlý Liste Türleri
+	-> Yýðýn Yapýsý 1{
+		- Tek Yönlü Baðlý Liste Yapýsý
+		- Ekleme
+		- Silme
+		- Listeleme
+		- Menü Yapýsý
+			- Ekleme
+			- Çýkarma
+			- Listeleme
+	}
+	-> Baðlý Liste 1{
+		- Tek Yönlü Baðlý Liste Yapýsý
+		- Veriler Kullanýcýdan
+		- Tek Veri Toplamlarý-Çift Veri Toplamlarý Ayrýmý
+		- Toplamý Büyük Olana Göre Sýralý Veya Ters Sýralý Kuralý
+	}
+	-> Baðlý Listeler Arasý Geçiþ{
+		- Yýðýn Yapýsý
+		- Kuyruk Yapýsý
+		- Tek Yönlü Baðlý Liste
+		- Dosya Ýþlemleri
+		- Menü Yapýsý
+			- Yýðýndan Sil Kuyruða Ekle
+			- Kuyruktan Sil Yýðýna Ekle
+			- Listele
+	}
+	-> Baðlý Listede Arama{
+		- Tek Yönlü Baðlý Liste Yapýsý
+	}
+	-> Ýki Balðý Listeden Ortak Eleman Arama{
+		- Recursive Sistem
+		- Tek Yönlü Baðlý Liste Yapýsý
+	}
+	-> Hash Fonksiyonu{
+		- Tek Yönlü Baðlý Liste ile Tablo Yapýsý
+		- Mod Deðerine Göre Ýþlem
+		- Veriler Dinamik
+	}
+	-> Mod Deðerine Göre Veri Ekleme{
+		- Tek Yönlü Baðlý Liste Yapýsý
+		- Dosya Ýþlemleri
+		- Menü Yapýsý
+			- Ekleme
+			- Listeleme
+			- Arama
+			- Silme
+			- Dosyaya Yazma
+	}
+	-> Aðýrlýklý Graf-Komþuluk Matrisi{
+		- Struct Yapýsý ile Graf Baðlantýsý
+		- Dfs Algoritmasý
+		- Prim Algoritmasý
+		- Dosya Ýþlemleri
+	}
+	-> Baðlý Liste 2{
+		- Tek Yönlü Baðlý Liste Yapýsý
+		- Menü Yapýsý
+			- Ekleme
+			- Silme
+			- Listeleme
+		- Seçimler harf kontrollü
+		- Her Ýþlemde Temizlenen Ekran
+	}
+	-> Baðlý Listede Ayný Olaný SÝl{
+		- Çift Yönlü Baðlý Liste Yapýsý
+		- Veriler Kullanýcýdan
+		- Benzer Verilerin Temizlenmesi
+	}
+	-> Baðlý Listede Rastgele Sayý Bulma{
+		- Tek Yönlü Baðlý Liste Yapýsý
+		- Veriler Dinamik Ve Rastgele
+		- Kullanýcýdan Sayý Tahmini Ýstenir
+		- Listede Varsa O Stun Ýþaretlenir
+	}
+	-> Baðlý Listede Tou Sayýsý{
+		- Tek Yönlü Baðlý Liste Yapýsý
+		- Veriler  Kullanýcýdan
+		- Tou Sayýsý Kontrolü
+	}
+	-> Baðlý Liste 3{
+		- Çift Yönlü Baðlý Liste Yapýsý
+		- Menü Yapýsý
+			- Kayýt Ekle
+			- Kayýt Listele
+			- Kayýt Sil
+		- Her Seçimde Ekran Temizlenir
+		- Veriler Kullanýcýdan
+	}
+	-> Baðlý Listede Sýralama 1{
+		- Çift Yönlü Baðlý Liste Yapýsý
+		- Veriler Kullanýcýdan
+	}
+	-> Dairesel Baðlý Listede Oyun{
+		- Tek Yönlü Baðlý Liste Yapýsý
+		- Veriler Dinamik
+		- Oyun Bilgisi Yorum Satýrýnda
+	}
+	-> Baðlý Listede String Ýþlem{
+		- Tek Yönlü Baðlý Liste Yapýsý
+		- Veriler Kullanýcýcdan
+		- Ýsimdeki Seçilen Karakterin
+			Kaç Defa Geçtiðinin Kontrolü
+	}
+	-> Baðlý Listede Sýralama 2{
+		- Tek Yönlü Baðlý Liste Yapýsý
+		- Veriler Dinamik
+	}
+	-> Baðlý Listede Palindrom{
+		- Tek Yönlü Baðlý Liste Yapýsý
+		- Veriler Kullanýcýdan
+		- Kelime Kontrolü Yýðýn Yapýsý
+	}
+}
+7-{ Rekürsive Örnekler
+	-> Faktöriyel
+	-> Fibonacci
+	-> Üs Alma
+	-> Toplama
+	-> Harf Elde Etme - X
+	-> Boþluklarý Temizleme
+	-> Aralarýnda Asal Bulma
+	-> Asal Bulma
+	-> N Sayýsýna Kadar Olan Sayýlarýn 
+		Ýstenilen Sayýya Bölüm Kontolü
+	-> E.B.O.B
+	-> E.K.O.K
+	-> Ýstenilen Karakterin Ýsimdeki Tekrarý
+	-> Ýstenilen Kelimenin Metindeki Tekrarý
+	-> Girilen Sayýnýn Rakamlarý Toplamý
+}
+8-{ Pointer Örnekler
+	-> Pointer 1
+	-> Pointer 2
+	-> Pointer Alan Hesabý
+	-> Pointer Swap Ýþlemi
+}
+9-{ Temel Öðrenme Denemeleri
+	-> 0 Durma Þartlý Sonsuz Girilen Sayýnýn Toplamý
+	-> 5-5 Matris Doldurma
+	-> Aralarýnda Asal
+	-> Ascii Tabloda Ýstenilen Aralýk
+	-> Baklava Dilimi
+	-> Büyük Küçük Harf Deðiþtirme
+	-> Çarpým Tablosu
+	-> Dört Katý Tersine Eþit Olan Sayý Kontrolü
+	-> E Harfi Yazdýrma
+	-> Girilen Karakterin Harf Sayý Kontrolü
+	-> Girilen Sayýlarýn Ýtenilen Ýki Sayýya Bölünebilirliði
+	-> Girilen Sayýlarýn Ýþaretine Göre Ayrýmý
+	-> Girilen Sayýlarýn Sýralanmasý Ve Ýlk Dört Sayýnýn Toplanmasý
+	-> Girilen Sayýnýn Ýkilik Tabandaki Karþýlýðý ve 0-1 Sayýsý
+	-> Girilen Sayýnýn Ýstenilen Biti
+	-> Girilen Sayýnýn Ramdeki Karþýlýðý
+	-> Girilen Sayýnýn Ramdeki Karþýlýðý ve 0-1 Sayýsý
+	-> Girilen Sayýnýn Toplamaya Göre Tersi
+	-> H Harfi Yazdýrma
+	-> Ýki Matrisin Toplamý Matris Görünümü
+	-> Ýkilik Tabandaki On Basamaklý Sayýnýn Onluk Tabandaki Karþýlýðý
+	-> Ýkilik Tabandan Onluk Tabana Dönüþüm
+	-> Kalp Çizimi
+	-> Kum Saati Çizimi
+	-> Kütle Ýndeks
+	-> Matris Oluþturma
+	-> N Sayýsýna Kadar Olan Sayýlarýn Ýstenilene Bölüm Kontrolü
+	-> N Tane Sayýnýn Ortalamasý ve Ortalamadan Büyük Olanlarýn Listelenmesi
+	-> Þifreleme
+	-> T Harfi Yazdýrma
+	-> Üçgen Çizimi
+	-> Yýðýn Yapýsý Metni Ters Yazma
+}
+10-{ OpenGL
+	-> Win7 DevC++ 4.9.9.2 de Yazýlanlar{
+		-> Poligon Üçgen
+		-> Kesik Çizgi
+		-> Çember
+		-> Renkli Çizim
+		-> Pencere Üzerinde Gezinme	
+	}
+	-> Windows 8.1 de Yazýlanlar{
+		-> GL_LINES
+		-> GL_POINTS
+		-> GL_POLYGON
+		-> GL_POLYGON with ROTATE
+		-> GL_POLYGON with TRANSLATED
+		-> GL_POLYGON with TRANSLATED and ROTATE
+		-> GL_QUAD_STRIP
+		-> GL_QUADS
+		-> GL_TRIANGLE_FAN
+		-> GL_TRIANGLE_STRIP
+		-> GL_TRIANGLES 1
+		-> GL_TRIANGLES 2
+	}
+}
+11-{ PROJE ve OYUNLAR
+	-> Analog Saat
+	-> Bayrak
+	-> System Data ( Arkadaþ Ýçin Doðum Günü Hediyesi)
+	-> Windows Ekranýnda Gezinti
+	-> Kim Milyoner Olmak Ýster
+	-> Yýlan Oyunu
+}
